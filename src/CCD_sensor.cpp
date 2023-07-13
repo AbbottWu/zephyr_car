dd25a6772a0ca7fc014c3763620b4ea16b2a5015
AbbottWu <AbbottDescriber@outlook.com>
2023-07-13 19:23:37 +0800

    due to the pico i2c driver problem, migrate to esp

---
+++ src/CCD_sensor.cpp
@@ -0,0 +1,4 @@
+// #include "CCD_sensor.h"
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/adc.h>
---
+++ src/CCD_sensor.h
@@ -0,0 +1,13 @@
+/**
+ * @file CCD_sensor.h
+ * @author AbbottWu (AbbottDescriber@outlook.com)
+ * ~/chinese @brief 线性CCD传感器 tsl1401 驱动
+ * @version 0.1
+ * @date 2023-07-13
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#pragma once
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
